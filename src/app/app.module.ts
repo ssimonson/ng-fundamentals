dfebfb10da0d376a8e23ffb9eb5c7a4a245b162d
Scott Simonson <ssimonson@practicevelocity.com>
2018-10-27 20:48:15 -0500

    Routing and Navigation Pages
    - Added Errors page and directory
    - Added barrels for the event section
    - Added services for the event section
    - Added user module and directory
    - Updated routes page to handle new error page and user module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,55 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
+import {
+
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from './events/index';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component,
     NavBarComponent
   ],
-  providers: [EventService, ToastrService],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    },
+    EventListResolver
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm('You have not saved this event, do you really want to cancel?');
+  }
+
+  return true;
+}
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'events-app',
   template: `
     <nav-bar></nav-bar>
-    <events-list></events-list>
+    <router-outlet></router-outlet>
   `
 })
 export class EventsAppComponent {
-  title = 'app';
+  title = 'Upcoming Events';
 }
